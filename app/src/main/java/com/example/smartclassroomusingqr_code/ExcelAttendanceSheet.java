53aacb6ce3e9cf1dde2f3eb3fd158c86196a5715
basit96 <pakkobra007@gmail.com>
2019-11-21 13:01:12 +0500

    Add attendance layout

---
+++ app/src/main/java/com/example/smartclassroomusingqr_code/ExcelAttendanceSheet.java
@@ -0,0 +1,14 @@
+package com.example.smartclassroomusingqr_code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExcelAttendanceSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excel_attendance_sheet);
+    }
+}
